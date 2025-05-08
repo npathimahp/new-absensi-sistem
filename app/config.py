3160bdae4146d037d1406d5c7a9c4c074c655b61
Nur Patimah <95556646+npathimahp@users.noreply.github.com>
2025-05-08 22:45:03 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,19 +7,9 @@ ENCODE_FILE = "EncodeFile.p"  # Path to the encoded faces file
 # Secret key for Flask session
 SECRET_KEY = os.environ.get("SECRET_KEY", "default-key")  # Change this to a random string
 
-FIREBASE_CREDENTIALS = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
-}
-
+FIREBASE_CREDENTIALS = json.loads(
+    base64.b64decode(os.getenv("FIREBASE_CREDENTIALS_BASE64")).decode("utf-8")
+)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 STORAGE_BUCKET = os.getenv("STORAGE_BUCKET")
