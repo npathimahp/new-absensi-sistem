2cf97f3afa2155fdfcd692115ec91b3227e2ce18
Nur Patimah <95556646+npathimahp@users.noreply.github.com>
2025-05-08 21:35:01 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,9 +8,6 @@ ENCODE_FILE = "EncodeFile.p"  # Path to the encoded faces file
 # Secret key for Flask session
 SECRET_KEY = os.environ.get("SECRET_KEY", "default-key")  # Change this to a random string
 
-# Firebase configuration details
-load_dotenv()
-
 FIREBASE_CREDENTIALS = json.loads(os.environ.get("FIREBASE_CREDENTIALS"))
 DATABASE_URL = os.environ.get("DATABASE_URL")
 STORAGE_BUCKET = os.environ.get("STORAGE_BUCKET")
