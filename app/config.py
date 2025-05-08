36ba1aef55385020ca158fccfec321bd80825261
Nur Patimah <95556646+npathimahp@users.noreply.github.com>
2025-05-08 22:34:53 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 import json
-from dotenv import load_dotenv
 
 # Cache for encoded faces
 ENCODE_FILE = "EncodeFile.p"  # Path to the encoded faces file
@@ -8,23 +7,19 @@ ENCODE_FILE = "EncodeFile.p"  # Path to the encoded faces file
 # Secret key for Flask session
 SECRET_KEY = os.environ.get("SECRET_KEY", "default-key")  # Change this to a random string
 
-# Firebase configuration details
-load_dotenv()
-
-import os
-
 FIREBASE_CREDENTIALS = {
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n")
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
     "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
 }
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 STORAGE_BUCKET = os.getenv("STORAGE_BUCKET")
